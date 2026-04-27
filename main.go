d11229ad9350c67ed5cbb986e3845d6cfd1dd2c0
ngnl-47 <ngamenl@163.com>
2026-04-27 10:49:50 +0800

    feat(组件): 事件总线改造

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
+import "strings"
+
 // 本模块为库与测试工程；订单系统行为验证请执行：
 //
 //	go test ./ordersys/...
 
-func main() {}
+func main() {
+	occupyKey := "xyjh_online_mix:legion:war:occupy:1:26032:10:-1:-1:11919"
+	println(strings.Contains(occupyKey, "260326"))
+}
